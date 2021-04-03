e1eebb7abe3f0a538b5c181e6a780adedf31c0cc
m-e-w <m-e-w@m-e-w.dev>
2021-04-03 16:53:43 +0000

    updated example template / doql

--- doql/cre_ru_all.sql
+++ doql/cre_ru_all.sql
@@ -4,7 +4,7 @@ WITH cre_data AS (
     FROM 
         view_credata_v2 
     WHERE
-        recommendation_type = 'ru'
+        recommendation_type = 'ru' AND vendor != 'VMware'
 )
 SELECT 
     (
@@ -34,7 +34,34 @@ SELECT
             cre_gcp.vendor = 'GCP' AND cre_main.device_fk = cre_gcp.device_fk AND recommendation_type = 'ru'
   
     ) instance_gcp,
-    cre_main.device_fk
+     (
+        SELECT 
+            cre_normal_aws.recommended_instance
+        FROM
+            view_Credata_v2 cre_normal_aws 
+        WHERE 
+            cre_normal_aws.vendor = 'AWS' AND cre_main.device_fk = cre_normal_aws.device_fk AND recommendation_type = 'regular'
+    ) instance_normal_aws,
+    cre_main.device_fk,
+    CASE
+        WHEN
+        (
+            SELECT 
+                cre_normal_aws.recommended_instance
+            FROM
+                view_Credata_v2 cre_normal_aws 
+            WHERE 
+                cre_normal_aws.vendor = 'AWS' AND cre_main.device_fk = cre_normal_aws.device_fk AND recommendation_type = 'regular'
+        ) = 
+        (
+            SELECT
+                cre_aws.recommended_instance
+            FROM
+                view_credata_v2 cre_aws
+            WHERE
+                cre_aws.vendor = 'AWS' AND cre_main.device_fk = cre_aws.device_fk AND recommendation_type = 'ru'
+  
+        ) THEN 'True' ELSE 'False' END AS right_sized 
 FROM
     cre_data cre_main
 
