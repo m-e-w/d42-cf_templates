27f1fdc267a6400adffd87be60a9208436c583cf
m-e-w <m-e-w@m-e-w.dev>
2021-04-03 22:55:53 +0000

    refactoring

--- starter.py
+++ starter.py
@@ -26,7 +26,66 @@ def find_endpoint(x):
         'appcomp' : '/api/1.0/custom_fields/appcomp/'
     }.get(x, 'Undefined')
 
-def module_bulk_field():
+def get_value(value, row):
+    if value.startswith('$'):
+        value = value[1:]
+        if row[value] is None:
+            value = 'None'
+        else:
+            value = row[value]
+    return value
+
+def module_bulk_field(row, unique_id, ci_type, cf_endpoint, template):
+    bulk_fields = []
+    data = {}
+   
+    if row[unique_id]:
+        if ci_type == 'device':
+            data['device_id'] = row[unique_id]
+        else:
+            data['id'] = row[unique_id]
+
+        for cf in template['custom_fields']:
+            key = cf
+            value = template['custom_fields'][cf]['value']
+            value = get_value(value, row)
+            kvp = key + ':' + value
+            bulk_fields.append(kvp)
+
+        data['bulk_fields'] = ",".join(bulk_fields)   
+        if debug or dry_run:
+            print('\tPayload: %s' % data)
+
+        if(dry_run == False):
+            response = device42_api._put_cf(data, cf_endpoint)
+            print('\tPut:\t%s\tResponse: %s' % (cf_endpoint,response))
+           
+def module_normal(row, unique_id, ci_type, cf_endpoint, template):
+    data = {}
+    if row[unique_id]:
+        if ci_type == 'device':
+            data['device_id'] = row[unique_id]
+        else:
+            data['id'] = row[unique_id]
+    for cf in template['custom_fields']:
+        value = template['custom_fields'][cf]['value']
+        data = dict(template['custom_fields'][cf])
+
+        if ci_type == 'device':
+            data['device_id'] = row[unique_id]
+        else:
+            data['id'] = row[unique_id]
+        data['key'] = cf
+        data['value'] = get_value(value, row)
+        
+        if debug or dry_run:
+            print('\tPayload: %s' % data)
+
+        if(dry_run == False):
+            response = device42_api._put_cf(data, cf_endpoint)
+            print('\tPut:\t%s\tResponse: %s' % (cf_endpoint,response))
+
+def main(module):
     for template in templates: 
         print('\nLoading template: %s' % template)
         saved_doql_name = templates[template]['saved_doql']
@@ -41,78 +100,17 @@ def module_bulk_field():
                 print(results)
             else:
                  for row in results: 
-                    bulk_fields = []
-                    data = {}
-                    if row[unique_id]:
-                        if ci_type == 'device':
-                            data['device_id'] = row[unique_id]
-                        else:
-                            data['id'] = row[unique_id]
-
-                        for cf in templates[template]['custom_fields']:
-                            key = cf
-                            value = templates[template]['custom_fields'][cf]['value']
-
-                            if value.startswith('$'):
-                                value = value[1:]
-                                if row[value] is None:
-                                    value = 'None'
-                                else:
-                                    value = row[value]
-                            kvp = key + ':' + value
-                            bulk_fields.append(kvp)
-
-                        data['bulk_fields'] = ",".join(bulk_fields)   
-                        if debug or dry_run:
-                            print('\tPayload: %s' % data)
-
-                        if(dry_run == False):
-                            response = device42_api._put_cf(data, cf_endpoint)
-                            print('\tPut:\t%s\tResponse: %s' % (cf_endpoint,response))
-
-    
-                
-def module_normal():
-    for template in templates: 
-        print('\nLoading template: %s' % template)
-        saved_doql_name = templates[template]['saved_doql']
-        unique_id = templates[template]['unique_id']
-        ci_type = templates[template]['ci_type']
-        cf_endpoint = find_endpoint(ci_type)
-       
-        if(cf_endpoint != 'Undefined'):
-            print('\nCalling saved DOQL query: %s' % saved_doql_name)
-            results = device42_api._get_doql(saved_doql_name)
-            
-            for row in results: 
-                for cf in templates[template]['custom_fields']:
-                    cf_value = templates[template]['custom_fields'][cf]['value']
-                    data = dict(templates[template]['custom_fields'][cf])
-
-                    if ci_type == 'device':
-                        data['device_id'] = row[unique_id]
-                    else:
-                        data['id'] = row[unique_id]
-                    data['key'] = cf
-                    if cf_value.startswith('$'):
-                        cf_value = cf_value[1:]
-                        data['value'] = row[cf_value]
-                   
-                    if debug or dry_run:
-                        print('\tPayload: %s' % data)
-
-                    if(dry_run == False):
-                        response = device42_api._put_cf(data, cf_endpoint)
-                        print('\tPut:\t%s\tResponse: %s' % (cf_endpoint,response))
-
-
-def main():
-    if use_bulk_fields:
-        module_bulk_field()
-    else:
-        module_normal()
+                     module(row, unique_id, ci_type, cf_endpoint, templates[template])
+        else:
+            print('API endpoint undefined, incorrect, or unsupported')
 
     
 if __name__ == '__main__':
-    main()
+    if use_bulk_fields:
+        main(module_bulk_field)
+    else:
+        main(module_normal)
+
+
+    
         
